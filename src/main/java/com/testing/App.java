e4aac95969f3408fd5f114bd5fe892da5673afcc
rdavis542 <rdavis542@verizon.net>
2023-07-17 18:14:03 -0400

    first local commit

---
+++ src/main/java/com/testing/App.java
@@ -0,0 +1,39 @@
+package com.testing;
+import java.security.*;
+import javax.crypto.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        System.out.println( "Hello World!" );
+
+        Logger logger = LogManager.getRootLogger();
+    	logger.trace("Configuration File Defined To Be :: "+System.getProperty("log4j.configurationFile"));
+
+        Signature sign = Signature.getInstance("SHA256withRSA");
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+        keyPairGen.initialize(4096);
+
+        KeyPair pair = keyPairGen.generateKeyPair();
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+
+        cipher.init(Cipher.ENCRYPT_MODE, pair.getPublic());
+
+        byte[] input = "This app will encrypt this scring and push it to a log file".getBytes();
+        cipher.update(input);
+
+        byte[] cipherText = cipher.doFinal();
+        System.out.println(new String(cipherText, "UTF8"));
+
+
+    }
+}
